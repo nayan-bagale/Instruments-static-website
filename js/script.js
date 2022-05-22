063d43fa5dd90c72c41e3b6df066bdb336a3e3e5
Nayan Bagale <nvbagale@yahoo.com>
2022-05-22 16:21:25 +0530

    major chnage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-
-function iconimage (window){
-    document.getElementsByClassName(".music-icon").src = "./img/icon/sound-waves.png"
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
 }
-
-iconimage(window)
