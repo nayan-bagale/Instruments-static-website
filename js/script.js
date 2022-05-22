165a579b9ddccac479cefabd757696ad371ced02
Nayan Bagale <nvbagale@yahoo.com>
2022-05-22 18:31:44 +0530

    js worked

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+function myFunction(instru) {
+    let dots = document.querySelector(`.block[id="${instru}"] .dots`);
+    let moreText = document.querySelector(`.block[id="${instru}"] .more`); 
+    let btnText = document.querySelector(`.block[id="${instru}"] .myBtn`);
+
+    console.log(dots);
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
+      btnText.textContent  = "Read more";
       moreText.style.display = "none";
     } else {
       dots.style.display = "none";
-      btnText.innerHTML = "Read less";
+      btnText.textContent  = "Read less";
       moreText.style.display = "inline";
     }
 }
